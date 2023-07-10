a8353d395161500c3f995df11e040d4c98a38766
Catoire-dev <cy.catoire.dev@gmail.com>
2023-07-10 18:02:43 +0200

    create login-singin view

---
+++ views/login.html.php
@@ -0,0 +1,13 @@
+<section id="page-log-sign-in">
+    <h1>login</h1>
+    <form action="POST" id="form-login" class="form">
+        <label for="username">E-mail / Pseudo</label>
+        <input type="text" id="username" required>
+
+        <label for="password">Mot de Passe</label>
+        <input type="password" id="password" required>
+
+        <input type="submit" value="Se Connecter" class="btn">
+    </form>
+    <p>Vous n'avez pas de compte? <a href="/sign_in">en crée un</a></p>
+</section>
---
+++ views/signin.html.php
@@ -0,0 +1,18 @@
+<section id="page-log-sign-in">
+    <h1>sign in</h1>
+    <form action="POST" id="form-signin" class="form">
+        <label for="pseudo">Pseudo</label>
+        <input type="text" id="pseudo" required>
+
+        <label for="email">E-mail</label>
+        <input type="text" id="email" required>
+
+        <label for="password">Mot de Passe</label>
+        <input type="password" id="password" required>
+
+        <label for="password-confirm">Confirmer Mot de Passe</label>
+        <input type="password" id="password-confirm" required>
+
+        <input type="submit" value="S'inscrire" class="btn">
+    </form>
+</section>
