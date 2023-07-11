e212eda9d7cf61e557646340b002521d7865601d
thomasmiotto <thomasmiotto2@gmail.com>
2023-07-11 16:05:58 +0200

    header and footer added

--- views/header.html.php
+++ views/header.html.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Styles/global.css">
     <link rel="stylesheet" href="Styles/page_log-sign-in.css">
-    <link rel="stylesheet" href="Styles/header.css">
+    <link rel="stylesheet" href="Styles/header_footer.css">
+    <link rel="stylesheet" href="Styles/accueil.css">
     <title>Document</title>
 </head>
 
@@ -14,8 +15,10 @@
 
 
     <header>
+        <div id="top_container">
         <div id="logo_search_container">
             <img src="../assets/logo.png" alt="moto_coolos" class="logo">
+            <p class="logo_txt">Le meilleur de l'équipement deux roues</p>
             <input type="text" placeholder="  Search...">
         </div>
         <div class="circle_container">
@@ -23,9 +26,16 @@
             <a href=""><img src="../assets/panier.png" alt=""></a>
             <a href=""><img src="../assets/email.png" alt=""></a>
         </div>
+        </div>
         <div class="red_container">
-            <h1>MOTO COOLOS</h1>
+            <h1>CATEGORIES</h1>
             <a href=""><img src="../assets/menu.png" alt=""></a>
         </div>
+        <ul>
+                <li><a href="">casques</a></li>
+                <li><a href="">accesoires</a></li>
+                <li><a href="">pièces moto</a></li>
+                <li><a href="">pneus</a></li>
+            </ul>
         </nav>
     </header>
