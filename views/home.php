8202b18997ea676cadc6134be2d2da90472d5383
Mario <marioaouane@gmail.com>
2023-07-12 12:31:52 +0200

    home

--- views/home.php
+++ views/home.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acceuil</title>
-    <link rel="stylesheet" href="/Styles/acceuil.css">
+    <link rel="stylesheet" href="/Styles/home.css">
     
 </head>
 
