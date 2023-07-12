2dc7cda1dba71ceec134ed3ba181b59fe3cd4aa9
Vidar <vidar_sather@hotmail.com>
2023-07-12 09:27:46 +0200

    router

--- index.php
+++ index.php
@@ -1,7 +1,38 @@
 <?php
 
 
-include_once __DIR__. '/views/header.html.php';
-include_once __DIR__. '/views/footer.html.php';
+include_once __DIR__ . '/views/header.html.php';
 
-?>
+session_start();
+
+use Controllers\StaticController;
+
+$route = $_SERVER['PATH_INFO'] ?? '/home';
+
+
+try {
+    switch ($route) {
+        case '/home':
+            StaticController::showHome();
+            break;
+
+        case '/contact':
+            StaticController::showContact();
+            break;
+
+        case '/cart':
+            StaticController::showCart();
+            break;
+
+        case '/login':
+            StaticController::showLogin();
+            break;
+
+        default:
+            //error
+    }
+} catch (Exception $e) {
+    //Error function
+}
+
+include_once __DIR__ . '/views/footer.html.php';
