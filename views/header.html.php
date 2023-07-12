768051650c91d702481d73cbe22592feec37aa0d
Vidar <vidar_sather@hotmail.com>
2023-07-12 11:08:34 +0200

    added some links and fixes some paths

--- views/header.html.php
+++ views/header.html.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="Styles/global.css">
     <link rel="stylesheet" href="Styles/page_log-sign-in.css">
     <link rel="stylesheet" href="Styles/header_footer.css">
@@ -17,26 +19,26 @@
 
     <header>
         <div id="top_container">
-        <div id="logo_search_container">
-            <img src="../assets/logo.png" alt="moto_coolos" class="logo">
-            <p class="logo_txt">Le meilleur de l'équipement deux roues</p>
-            <input type="text" placeholder="  Search...">
-        </div>
-        <div class="circle_container">
-            <a href=""><img src="../assets/profil.png" alt=""></a>
-            <a href=""><img src="../assets/panier.png" alt=""></a>
-            <a href=""><img src="../assets/email.png" alt=""></a>
-        </div>
+            <div id="logo_search_container">
+                <img src="../Assets/logo.png" alt="moto_coolos" class="logo">
+                <p class="logo_txt">Le meilleur de l'équipement deux roues</p>
+                <input type="text" placeholder="  Search...">
+            </div>
+            <div class="circle_container">
+                <a href="profile"><img src="../Assets/profil.png" alt=""></a>
+                <a href="cart"><img src="../Assets/panier.png" alt=""></a>
+                <a href="contact"><img src="../Assets/email.png" alt=""></a>
+            </div>
         </div>
         <div class="red_container">
             <h1>CATEGORIES</h1>
-            <a href=""><img src="../assets/menu.png" alt=""></a>
+            <a href=""><img src="../Assets/menu.png" alt=""></a>
         </div>
         <ul>
-                <li><a href="">casques</a></li>
-                <li><a href="">accesoires</a></li>
-                <li><a href="">pièces moto</a></li>
-                <li><a href="">pneus</a></li>
-            </ul>
+            <li><a href="">casques</a></li>
+            <li><a href="">accesoires</a></li>
+            <li><a href="">pièces moto</a></li>
+            <li><a href="">pneus</a></li>
+        </ul>
         </nav>
     </header>
