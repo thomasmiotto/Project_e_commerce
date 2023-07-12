4845bef2ecb9463a2ef9805ffe73db374028d63e
thomasmiotto <thomasmiotto2@gmail.com>
2023-07-12 11:15:44 +0200

    added

---
+++ views/compte_client.html.php
@@ -0,0 +1,19 @@
+<link rel="stylesheet" href="../Styles/compte_client.css">
+<link rel="stylesheet" href="../Styles/global.css">
+
+<section id="compte">
+    <h1>Mon compte</h1>
+    <div class="card_user">
+        <div class="pp_name">
+            <img src="../Assets/profil.png" alt="profil">
+            <h2>NOM DE L'UTILISATEUR</h2>
+        </div>
+        <div class="btn_container">
+            <button class="btn">MODIFIER</button>
+            <button class="btn">SUPPRIMER</button>
+        </div>
+    </div>
+    <h1>Mes informations</h1>
+    <p>Email : monEmail</p>
+    <p>Pseudo : NomDuPseudo</p>
+</section>
