e1a3e473f9df2869954f6cc4546d14001d2413e8
thomasmiotto <thomasmiotto2@gmail.com>
2023-07-12 14:10:54 +0200

    link css added and border added to the searchbar

--- views/header.html.php
+++ views/header.html.php
@@ -9,8 +9,11 @@
     <link rel="stylesheet" href="Styles/global.css">
     <link rel="stylesheet" href="Styles/page_log-sign-in.css">
     <link rel="stylesheet" href="Styles/header_footer.css">
-    <link rel="stylesheet" href="Styles/accueil.css">
-    <link rel="stylesheet" href="../Styles/product_list.css">
+    <link rel="stylesheet" href="Styles/home.css">
+    <link rel="stylesheet" href="Styles/product_list.css">
+    <link rel="stylesheet" href="Styles/product_in_cart.css">
+    <link rel="stylesheet" href="Styles/cart.css">
+    <link rel="stylesheet" href="Styles/index.css">
     <title>Document</title>
 </head>
 
@@ -22,7 +25,7 @@
             <div id="logo_search_container">
                 <img src="../Assets/logo.png" alt="moto_coolos" class="logo">
                 <p class="logo_txt">Le meilleur de l'équipement deux roues</p>
-                <input type="text" placeholder="  Search...">
+                <input type="text"  class="searchbar" placeholder="  Search...">
             </div>
             <div class="circle_container">
                 <a href="profile"><img src="../Assets/profil.png" alt=""></a>
